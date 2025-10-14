e484a0217999a26f82ce24feba8b1d54a77a20f1
Lucy Wingard <lucywingard97@gmail.com>
2025-10-13 18:48:06 -0700

    add draft mode

--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -2,8 +2,15 @@
 import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
 
-export async function GET() {
-  (await draftMode()).enable()
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const secret = searchParams.get('secret')
+  
+  // Add a secret to prevent unauthorized access
+  if (secret !== process.env.SANITY_PREVIEW_SECRET) {
+    return new Response('Invalid token', { status: 401 })
+  }
 
+  (await draftMode()).enable()
   redirect('/')
-}
+}  
